17076c9eba743629c421aa3c594829988457598d
danielld75 <danielld@poczta.fm>
2018-01-23 15:47:07 +0100

    First comment

--- components/Contacts.js
+++ components/Contacts.js
@@ -2,12 +2,12 @@ var Contacts = React.createClass({
   propTypes: {
     items: React.PropTypes.array.isRequired
   },
-  render: function() {
-    var contacts = this.props.items.map(function(contact) {
-      return React.createElement(Contact, {item: contact, key: contact.id});
+  render: function () {
+    var contacts = this.props.items.map(function (contact) {
+      return <Contact item={contact} key={contact.id}/>
     });
     return (
-      React.createElement('ul', {className: 'contactList'}, contacts)
-    )
+      <ul className={'contactList'}> {contacts} </ul>
+    );
   }
 });
